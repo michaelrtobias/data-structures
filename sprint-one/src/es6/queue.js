d06129a7aee3e243054b71150f72c2d4d6e768ce
Michael Tobias <michaelremitobias@gmail.com>
2020-08-20 11:04:13 -0700

    "Finished ES6"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,23 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.position = 1;
+    this.remove = 1;
+  }
+
+  enqueue(value) {
+    this.storage[this.position] = value;
+    this.position++;
+  }
+  dequeue() {
+    var deletedValue = this.storage[this.remove];
+    delete this.storage[this.remove];
+    this.remove++;
+    return deletedValue;
+  }
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.position = 0;
+  }
+
+
+  push(value) {
+    this.position++;
+    this.storage[this.position] = value;
+  }
+  pop() {
+    var deletedValue = this.storage[this.position];
+    delete this.storage[this.position];
+    this.position--;
+    return deletedValue;
+  }
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
