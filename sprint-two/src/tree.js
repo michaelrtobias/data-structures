d59a97414926d43e129e4793e59e75e56129dd9c
Michael Tobias <michaelremitobias@gmail.com>
2020-08-21 11:29:08 -0700

    "Finished Tree"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
+//functional shared
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  extend(newTree, treeMethods);
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +13,30 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var addedValue = Tree(value);
+  this.children.push(addedValue);
 };
 
 treeMethods.contains = function(target) {
+  var doesContain = false;
+  var amberAlert = function(obj) {
+    if (obj.value === target) {
+      doesContain = true;
+    }
+    if (obj.children.length > 0) {
+
+      obj.children.forEach(element => amberAlert(element));
+    }
+  };
+
+  amberAlert(this);
+  return doesContain;
+};
+
+var extend = function(obj, methods) {
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
 };
 
 
@@ -21,3 +44,6 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// var testTree = Tree('test');
+
+// console.log(testTree);
