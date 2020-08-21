7dde2b6280e7d692821cc9673bdd1e3aefe134e1
Michael Tobias <michaelremitobias@gmail.com>
2020-08-20 17:35:50 -0700

    "Finished Linked List"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,45 @@
+//Using functional method of classes
+
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    var currentNode;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      currentNode = this.head;
+      this.tail = newNode;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+    var currentHead = list.head.value;
+    list.head = list.head.next;
+    return currentHead;
   };
 
   list.contains = function(target) {
+    var doesContain = false;
+    var currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        doesContain = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return doesContain;
   };
 
   return list;
