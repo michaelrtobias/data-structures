8a682ce2a8aa3498bfa623bd7e6d7d4c742b92f9
Michael Tobias <michaelremitobias@gmail.com>
2020-08-22 10:12:46 -0700

    "FInished Set"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
+//prototypal
+
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this.storage.indexOf(item) !== -1;
 };
 
 setPrototype.remove = function(item) {
+  var index = this.storage.indexOf(item);
+  this.storage.splice(index, 1);
 };
 
 /*
